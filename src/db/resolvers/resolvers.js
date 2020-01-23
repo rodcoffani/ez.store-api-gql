ac4ca6eec0b1e7ce4f66bd566cd5040fc09c5f34
rodcoffani <rodrigo.coffani@gmail.com>
2020-01-23 17:29:00 -0300

    queries fixed

--- src/db/resolvers/resolvers.js
+++ src/db/resolvers/resolvers.js
@@ -11,8 +11,8 @@ module.exports = {
                 });
         },
         user: (parent, args, { db }) => {
-            const query = `SELECT * FROM users WHERE id=${args.id}`;
-            return db.query(query)
+            const query = `SELECT * FROM users WHERE id = $1`;
+            return db.query(query, [args.id])
                 .then(data => {
                     return data.rows[0];
                 })
@@ -31,10 +31,10 @@ module.exports = {
                 });
         },
         wallet: (parent, args, { db }) => {
-            const query = `SELECT * FROM wallets WHERE id = ${args.id}`;
-            return db.query(query)
+            const query = `SELECT * FROM wallets WHERE id = $1`;
+            return db.query(query, [args.id])
                 .then(data => {
-                    return data;
+                    return data.rows[0];
                 })
                 .catch(err => {
                     return 'Error! The error is ', err;
@@ -44,22 +44,21 @@ module.exports = {
             const query = `SELECT * FROM donations`;
             return db.query(query)
                 .then(data => {
-                    return data;
+                    return data.rows;
                 })
                 .catch(err => {
                     return 'Error! The error is ', err;
                 });
         },
         donation: (parent, args, { db }) => {
-            const query = `SELECT * FROM wallets WHERE id = ${args.id}`;
-            return db.query(query)
+            const query = `SELECT * FROM donations WHERE id = $1`;
+            return db.query(query, [args.id])
                 .then(data => {
-                    return data;
+                    return data.rows[0];
                 })
                 .catch(err => {
                     return 'Error! The error is ', err;
                 });
         },
-        teste: () => 'Aqui é só pra testar'
     }
 };
